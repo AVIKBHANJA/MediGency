5ad114381daa0fb35e95dc3add3461e36c90eb80
Avik Bhanja <avikbhanja3@gmail.com>
2024-03-16 15:01:59 +0530

    created render method from app

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from "react";
+
+import Layout from "./Layout.jsx";
+import Home from "./Home.jsx";
+import Aboutus from "./Aboutus.jsx";
+import Services from "./Services.jsx";
+import Contactus from "./Contactus.jsx";
+import Feedback from "./Feedback.jsx";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+      <Route path="aboutus" element={<Aboutus />} />
+      <Route path="services" element={<Services />} />
+      <Route path="contactus" element={<Contactus />} />
+      <Route path="feedback" element={<Feedback />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
